6ba1d90325fa24226c97536fc4b4fefc2a87ea8f
Carlos Benavides <carlosbvz@gmail.com>
2024-10-04 17:20:22 -0600

    updating auth rules

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -27,10 +27,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "userPool",
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
+    defaultAuthorizationMode: "userPool"
   },
 });
 
